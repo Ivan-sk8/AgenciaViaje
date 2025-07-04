faf07cde05c15a0ffa93b73bb504c2f89f22f5c6
Ivan-sk8 <2302146@utrivieramaya.edu.mx>
2025-07-03 22:48:21 -0500

    first commit

---
+++ src/main/java/com/trivago/agenciaviaje/modelo/AgenciaViaje.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.trivago.agenciaviaje.modelo;
+
+/**
+ *
+ * @author ivan_
+ */
+public class AgenciaViaje {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
